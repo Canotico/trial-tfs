9f5c7f853928f7d6b508d95379d135817904175e
Cano <alejandro.cannizzo@gmail.com>
2024-04-18 17:14:03 -0400

    Added Trial Frost Spell for Q5

---
+++ vc17/data/spells/scripts/attack/trial_frost_spell.lua
@@ -0,0 +1,16 @@
+local combat = Combat()
+combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
+combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
+combat:setArea(createCombatArea(AREA_CIRCLE3X3))
+
+function onGetFormulaValues(player, level, magicLevel)
+	local min = (level / 5) + (magicLevel * 3) + 32
+	local max = (level / 5) + (magicLevel * 9) + 40
+	return -min, -max
+end
+
+combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
+
+function onCastSpell(creature, variant)
+	return combat:execute(creature, variant)
+end
